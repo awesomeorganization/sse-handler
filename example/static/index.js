f52e7b491257681c4ee1d96fdfae7e99948f9cae
sergeysedoy97 <48452323+sergeysedoy97@users.noreply.github.com>
2021-02-11 15:11:02 +0300

    Update index.js
    IIFE

--- example/static/index.js
+++ example/static/index.js
@@ -1,15 +1,15 @@
-/* global document EventSource */
+(function () {
+  'use strict'
 
-'use strict'
+  const [withoutEvent, withEvent] = document.querySelectorAll('textarea')
 
-const [withoutEvent, withEvent] = document.querySelectorAll('textarea')
+  const source = new EventSource('/sse')
 
-const source = new EventSource('/sse')
+  source.addEventListener('message', ({ data }) => {
+    withoutEvent.append(data + '\n')
+  })
 
-source.addEventListener('message', ({ data }) => {
-  withoutEvent.append(data + '\n')
-})
-
-source.addEventListener('someEvent', ({ data }) => {
-  withEvent.append(data + '\n\n')
-})
+  source.addEventListener('someEvent', ({ data }) => {
+    withEvent.append(data + '\n\n')
+  })
+})()
